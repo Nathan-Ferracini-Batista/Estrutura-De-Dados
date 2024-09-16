45d8ef9dc00daf1d76facbf477085718db3d89b7
Nathan-Ferracini-Batista <nferracini38@gmail.com>
2024-09-16 10:59:02 -0300

    codigo decimal para binario

---
+++ codigos/01-HelloWorld.js
@@ -0,0 +1,52 @@
+var num = 1; // {1}
+num = 3; // {2}
+var price = 1.5; // {3}
+var myName = 'Packt'; // {4}
+var trueValue = true; // {5}
+var nullVar = null; // {6}
+var und; // {7}
+console.log(myName);
+console.log('num: ' + num);
+console.log('myName: ' + myName);
+console.log('trueValue: ' + trueValue);
+console.log('price: ' + price);
+console.log('nullVar: ' + nullVar);
+console.log('und: ' + und);
+
+var myVariable = 'global';
+myOtherVariable = 'global';
+function myFunction() {
+  var myVariable = 'local';
+  return myVariable;
+}
+function myOtherFunction() {
+  myOtherVariable = 'local';
+  return myOtherVariable;
+}
+console.log(myVariable); //{1}
+console.log(myFunction()); //{2}
+console.log(myOtherVariable); //{3}
+console.log(myOtherFunction()); //{4}
+console.log(myOtherVariable); //{5}
+
+var num = 0; // {1}
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++;
+num--;
+num += 1; // {2}
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+console.log('num == 1 : ' + (num == 1)); // {3}
+console.log('num === 1 : ' + (num === 1));
+console.log('num != 1 : ' + (num != 1));
+console.log('num > 1 : ' + (num > 1));
+console.log('num < 1 : ' + (num < 1));
+console.log('num >= 1 : ' + (num >= 1));
+console.log('num <= 1 : ' + (num <= 1));
+console.log('true && false : ' + (true && false)); // {4}
+console.log('true || false : ' + (true || false));
+console.log('!true : ' + !true);
